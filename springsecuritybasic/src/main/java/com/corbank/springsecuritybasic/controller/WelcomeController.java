be8d266d1b87ccb79abf5e757488abb7ac628a4a
Miguel Garcia Mérida <miguel.garciameri@atmira.com>
2023-06-19 14:14:23 +0200

    feat(main) Primer push con el basic Authentication method

---
+++ springsecuritybasic/src/main/java/com/corbank/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.corbank.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to SpringBoot application with basic security";
+    }
+}
